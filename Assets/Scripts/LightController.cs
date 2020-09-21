f59c94c9224cecdea08caedfab1b1d36d4f1188c
AG4W <lightarts@live.se>
2020-09-21 13:34:21 +0200

    Stuff

---
+++ Assets/Scripts/LightController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    [SerializeField]Light[] lights;
+
+    float flickerLength = .1f;
+    float flickerTimer = 0f;
+    
+    Light current;
+
+    void Start()
+    {
+        current = lights[Random.Range(0, lights.Length)];
+        current.gameObject.SetActive(false);
+
+        flickerLength = Random.Range(.05f, .2f);
+        flickerTimer = 0f;
+    }
+    void Update()
+    {
+        flickerTimer += Time.deltaTime;
+
+        if (flickerTimer >= flickerLength)
+        {
+            current.gameObject.SetActive(true);
+            current = lights[Random.Range(0, lights.Length)];
+
+            flickerLength = Random.Range(.05f, .2f);
+            flickerTimer = 0f;
+
+            current.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField]float speed;
+
+    void Update()
+    {
+        Cursor.lockState = Input.GetKey(KeyCode.Mouse0) ? CursorLockMode.Locked : CursorLockMode.None;
+        Cursor.visible = !Input.GetKey(KeyCode.Mouse0);
+
+        if (Input.GetKey(KeyCode.Mouse0))
+            this.transform.eulerAngles += new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0f) * speed * Time.deltaTime;
+    }
+}
