c3eb352ad6d9e8f04ab58af0c93224cbe24deddb
markstrm <markus.bergstrom@outlook.com>
2020-09-22 16:43:47 +0200

    hide/show text on click

--- VisualStudioCode/public/scripts/showhideelements.js
+++ VisualStudioCode/public/scripts/showhideelements.js
@@ -1,13 +1,43 @@
-const section1 = document.getElementById('section01')
+const fl = document.getElementById('fl');
+const flinfo = document.getElementById('fl-info');
 
-function show_hide(){
-    var x = document.getElementById("section1");
-    if(x.style.display === "none"){
-        section1.classList.remove('hide-section1')
-        x.style.display = "block";
-    }else{
-        section1.classList.add('hide-section1')
-        x.style.display = "none";
-    }
+const pl = document.getElementById('pl');
+const plinfo = document.getElementById('pl-info');
 
-}
+const ol = document.getElementById('ol');
+const olinfo = document.getElementById('ol-info');
+
+const tl = document.getElementById('tl');
+const tlinfo = document.getElementById('tl-info');
+
+fl.addEventListener("click", (evt) => {
+    flinfo.style.display = 'inline-block';
+    plinfo.style.display = 'none';
+    olinfo.style.display = 'none';
+    tlinfo.style.display = 'none';
+
+});
+
+pl.addEventListener("click", (evt) => {
+    plinfo.style.display = 'inline-block';
+    flinfo.style.display = 'none';
+    olinfo.style.display = 'none';
+    tlinfo.style.display = 'none';
+
+});
+
+ol.addEventListener("click", (evt) => {
+    olinfo.style.display = 'inline-block';
+    flinfo.style.display = 'none';
+    plinfo.style.display = 'none';
+    tlinfo.style.display = 'none';
+
+});
+
+tl.addEventListener("click", (evt) => {
+    tlinfo.style.display = 'inline-block';
+    flinfo.style.display = 'none';
+    plinfo.style.display = 'none';
+    olinfo.style.display = 'none';
+
+});
